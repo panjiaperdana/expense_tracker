ee02d8a2bac9322b7d6647cd4ce22d12fb20c6ee
Panji Perdana <panjiaperdana@gmail.com>
2025-12-18 16:41:29 +0700

    db.py: create connection & session, test_db.py: testing the connection & session

---
+++ app/db.py
@@ -0,0 +1,7 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from app.config import settings
+
+# Connect to your existing PostgreSQL database
+engine = create_engine(settings.DATABASE_URL, echo=False, future=True)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False, future=True)
--- test/test_db.py
+++ test/test_db.py
@@ -1,6 +1,8 @@
 import pytest
 from sqlalchemy import create_engine
 from app.config import settings  # import your Settings dataclass
+from app.db import SessionLocal
+from app.models import Account
 
 def test_database_connection():
     engine = create_engine(settings.DATABASE_URL)
@@ -10,3 +12,7 @@ def test_database_connection():
             print("✅ Connected successfully to:", conn)
     except Exception as e:
         pytest.fail(f"❌ Connection failed: {e}")
+
+def get_accounts():
+    with SessionLocal() as session:
+        return session.query(Account).all()
